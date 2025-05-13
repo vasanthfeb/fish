ed270eafde45a81c3e1421710a154e225246270c
vasanthfeb <vasanthfeb18@gmail.com>
2025-05-13 19:27:59 +0530

    first commit master

---
+++ _templates/_header.php
@@ -0,0 +1,18 @@
+<nav class="navbar navbar-dark bg-dark">
+    <div class="container">
+        <div class="navbar-brand">FISH</div>
+        <button class="navbar-toggler" data-bs-toggle="offcanvas" data-bs-target="#off"><span class="navbar-toggler-icon"></span></button>
+
+        <div class="offcanvas-end offcanvas" id="off">
+            <div class="offcanvas-header">
+                <div class="offcanvas-title">fISH</div>
+                <button class="btn-close" data-bs-dismiss="offcanvas"></button>
+            </div>
+            <div class="offcanvas-body">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci expedita sequi ab dignissimos eaque alias! Perferendis, ratione velit accusamus maxime ea quidem veritatis commodi minima voluptatibus ab earum consequatur itaque?</p>
+            </div>
+
+        </div>
+    </div>
+
+</nav>
