c5c8e50e25f1e8cd8242905cb78cbc3ec3b099d3
vasanthfeb <vasanthfeb18@gmail.com>
2025-05-14 20:18:15 +0530

    bg color master

--- _templates/_header.php
+++ _templates/_header.php
@@ -1,9 +1,10 @@
-<nav class="navbar navbar-dark bg-dark">
+<nav class="navbar navbar-dark nav_bg">
     <div class="container">
         <div class="navbar-brand">FISH</div>
         <button class="navbar-toggler" data-bs-toggle="offcanvas" data-bs-target="#off"><span class="navbar-toggler-icon"></span></button>
 
         <div class="offcanvas-end offcanvas" id="off">
+            <div class="bg-blur"></div>
             <div class="offcanvas-header">
                 <div class="offcanvas-title">fISH</div>
                 <button class="btn-close" data-bs-dismiss="offcanvas"></button>
@@ -31,6 +32,9 @@
         background-position: center;
         background-size: cover;
         overflow: hidden;
-        backdrop-filter: blur(90px) !important;
+       
+    }
+    .nav_bg{
+        background-color: #003280;
     }
 </style>
