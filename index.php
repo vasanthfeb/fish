e58eb4a9e03a9e7219a136f22238a3bffb6df09e
vasanthfeb <vasanthfeb18@gmail.com>
2025-05-15 05:36:23 +0530

    nav master

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Fish</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css" integrity="sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <?php
+  include('./_templates/_head.php');
+
+  ?>
 </head>
 <body>
     
@@ -12,14 +12,15 @@
 <header>
     <?php
     include_once('./_templates/_header.php');
-
     ?>
 </header>
 
 
 
 
+<?php
+include_once('./_templates/_footer_links.php')
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.min.js" integrity="sha512-ykZ1QQr0Jy/4ZkvKuqWn4iF3lqPZyij9iRv6sGqLRdTPkY69YX6+7wvVGmsdBbiIfN/8OdsI7HABjvEok6ZopQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+?>
 </body>
 </html>
